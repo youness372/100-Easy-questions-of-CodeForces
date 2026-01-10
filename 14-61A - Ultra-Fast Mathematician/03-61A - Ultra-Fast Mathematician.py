699391ab345266921a8504fb6dda38129071c09c
younsse347 <younessagrine2@gmail.com>
2026-01-10 20:36:42 +0100

    Create 03-61A - Ultra-Fast Mathematician.py

---
+++ 14-61A - Ultra-Fast Mathematician/03-61A - Ultra-Fast Mathematician.py
@@ -0,0 +1,11 @@
+n =  input()    
+m =  input ()   
+result =  ""
+for i in  range (len(n))  :      
+    if  n[i] == m [i] :  
+        result += "0"   
+    else :   
+        result += "1"      
+print  (result)   
+
+#Easyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
